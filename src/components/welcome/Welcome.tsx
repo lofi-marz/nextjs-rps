9ce61e602fbea8670b9db0fafae1e25d7153a702
lofi-marz <marzipant.e@gmail.com>
2022-11-09 10:08:27 +0000

    UI tweaks to menu

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -1,58 +1,84 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { FaMagic } from 'react-icons/fa';
 import { AnimatePresence, motion } from 'framer-motion';
 
 export function Welcome() {
     return (
-        <div className="flex h-screen w-full flex-col items-center justify-center dark:bg-dark-900 dark:text-white">
+        <div className="relative flex h-screen w-full flex-col items-center justify-center dark:bg-dark-900 dark:text-white">
             <h1 className="text-4xl">Quick RPS</h1>
-            <form className="flex flex-col items-center justify-center gap-5 p-10 text-3xl">
+            <motion.form
+                className="flex flex-col items-center justify-center gap-5 p-10 text-3xl"
+                layout>
                 <AnimatePresence>
-                    <CreateGameButton />
+                    <CreateGame />
                 </AnimatePresence>
                 <input
                     placeholder="Join"
                     className="w-full rounded border-2 border-white p-4 text-center placeholder:text-dark-300 dark:bg-dark-900 dark:text-white"></input>
-            </form>
+            </motion.form>
         </div>
     );
 }
 
-function CreateGameButton() {
-    const [code, setCode] = useState('');
-    const codes = ['aaaaa', 'bbbbb', 'ccccc', 'ddddd', 'eeeee'];
-    if (code)
-        return (
-            <motion.div
-                key="code"
-                className={'flex w-full overflow-clip rounded dark:bg-dark-800'}
-                initial={{ scaleX: 0 }}
-                animate={{ scaleX: 1 }}
-                exit={{ scaleX: 0 }}>
-                <AnimatePresence mode="wait">
-                    <motion.div
-                        key={code}
-                        className="grow p-5 text-center"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}>
-                        {code}
-                    </motion.div>
-                </AnimatePresence>
+const codes = ['aaaaa', 'bbbbb', 'ccccc', 'ddddd', 'eeeee'];
 
-                <button
-                    className="aspect-square h-full bg-primary p-5"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        setCode(codes[Math.floor(Math.random() * 5)]);
-                    }}>
-                    <FaMagic />
-                </button>
-            </motion.div>
-        );
+function RedoCodeButton({
+    setCode,
+}: {
+    setCode: Dispatch<SetStateAction<string>>;
+}) {
+    return (
+        <button
+            className="aspect-square h-full bg-primary p-5"
+            onClick={(e) => {
+                e.preventDefault();
+                setCode(codes[Math.floor(Math.random() * 5)]);
+            }}>
+            <FaMagic />
+        </button>
+    );
+}
+
+function Code({
+    code,
+    setCode,
+}: {
+    code: string;
+    setCode: Dispatch<SetStateAction<string>>;
+}) {
+    return (
+        <motion.div
+            key="code"
+            layout
+            className={'flex w-full overflow-clip rounded dark:bg-dark-800'}
+            initial={{ scaleX: 0 }}
+            animate={{ scaleX: 1 }}
+            exit={{ scaleX: 0 }}>
+            <AnimatePresence mode="wait">
+                <motion.div
+                    className="grow p-5 text-center"
+                    key={code}
+                    layoutId="create-room"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}>
+                    {code}
+                </motion.div>
+            </AnimatePresence>
+            <RedoCodeButton setCode={setCode} />
+        </motion.div>
+    );
+}
+
+function CreateGameButton({
+    setCode,
+}: {
+    setCode: Dispatch<SetStateAction<string>>;
+}) {
     return (
         <motion.button
             key="no-code"
+            layoutId="create-room"
             className="w-full rounded p-5 dark:bg-dark-800"
             initial={{ scaleX: 0 }}
             animate={{ scaleX: 1 }}
@@ -64,4 +90,11 @@ function CreateGameButton() {
             Create Game
         </motion.button>
     );
+}
+
+function CreateGame() {
+    const [code, setCode] = useState('');
+
+    if (code) return <Code code={code} setCode={setCode} />;
+    return <CreateGameButton setCode={setCode} />;
 }
